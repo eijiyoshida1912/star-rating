1aa6cfd1081482ef638205b065db9d06f54f78c4
EijiYoshida <eijiyoshida1912@gmail.com>
2022-04-27 22:01:57 +0900

    6.1.1まで終わり

---
+++ src/StarRating.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import Star from "../src/Star";
+
+export default function StarRating({ totalStars = 5 }) {
+  const [selectedStars, setSelectedStars] = useState(3);
+  return (
+    <>
+      {[...Array(totalStars)].map((n, i) => (
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => setSelectedStars(i + 1)}
+        />
+      ))}
+      <p>
+        {selectedStars} of {totalStars} stars
+      </p>
+    </>
+  );
+}
